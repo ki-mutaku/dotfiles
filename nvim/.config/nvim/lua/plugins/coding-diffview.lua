9c9b77160b094f61bda4f0aadc061618f20231e9
takuya <e245745@ie.u-ryukyu.ac.jp>
2026-05-21 09:08:16 +0900

    現在のファイルの変更内容を表示できるように修正

--- nvim/.config/nvim/lua/plugins/coding-diffview.lua
+++ nvim/.config/nvim/lua/plugins/coding-diffview.lua
@@ -8,6 +8,6 @@ return {
     {mode = "n", "<leader>hh", "<cmd>DiffviewOpen HEAD~1<CR>", desc = "1つ前とのdiff"},
     {mode = "n", "<leader>hf", "<cmd>DiffviewFileHistory %<CR>", desc = "ファイルの変更履歴"},
     {mode = "n", "<leader>hc", "<cmd>DiffviewClose<CR>", desc = "diffの画面閉じる"},
-    {mode = "n", "<leader>hd", "<cmd>Diffview<CR>", desc = "コンフリクト解消画面表示"},
+    {mode = "n", "<leader>hd", "<cmd>DiffviewOpen<CR>", desc = "コンフリクト解消画面表示"},
   },
 }
