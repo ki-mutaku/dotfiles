01c2badd36515a798a26e2d5807fe1d597e671ba
takuya <e245745@ie.u-ryukyu.ac.jp>
2026-05-29 10:10:13 +0900

    lazyvimの自動アップデートチェッカーをオフ

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
 	install = { colorscheme = { "habamax" } },
-	-- automatically check for plugin updates
-	checker = { enabled = true },
+	-- Disable automatic update checks to prevent unintended network requests.
+	-- Update manually with :Lazy update after reviewing changelogs.
+	checker = { enabled = false },
 })
