e78cd8471fcc2f6b3d8681e4d69bee9b3680e97e
takuya <e245745@ie.u-ryukyu.ac.jp>
2026-08-31 11:58:43 +0900

    catppuccin mochaに変更

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,30 +1,30 @@
 return {
-  "folke/tokyonight.nvim",
-  enabled = true, 
-  lazy = false,
-  priority = 1000,
-  opts = {
-    -- 1. カラーパレットそのものを書き換える
-    colors = {
-      all = {
-        dark5 = "#AAAAAA", -- 相対行数の基本色に使われる変数を落ち着いた灰色に固定
-      }
-    },
-    -- 2. ハイライトの上書き設定
-    on_highlights = function(hl, c)
-      -- 現在行の行番号 (黄色)
-      hl.CursorLineNr = {
-        fg = "#EAD330",
-        bold = true,
-      }
-      -- 相対行数 (落ち着いた灰色)
-      hl.LineNr = { fg = "#AAAAAA" }
-      hl.LineNrAbove = { fg = "#AAAAAA" }
-      hl.LineNrBelow = { fg = "#AAAAAA" }
-    end,
-  },
-  config = function(_, opts)
-    require("tokyonight").setup(opts)
-    vim.cmd[[colorscheme tokyonight]]
-  end
+	"catppuccin/nvim",
+	enabled = true,
+	lazy = false,
+	priority = 1000,
+	opts = {
+		-- 1. カラーパレットそのものを書き換える
+		colors = {
+			all = {
+				dark5 = "#AAAAAA", -- 相対行数の基本色に使われる変数を落ち着いた灰色に固定
+			},
+		},
+		-- 2. ハイライトの上書き設定
+		on_highlights = function(hl, c)
+			-- 現在行の行番号 (黄色)
+			hl.CursorLineNr = {
+				fg = "#E4C068",
+				bold = true,
+			}
+			-- 相対行数 (落ち着いた灰色)
+			hl.LineNr = { fg = "#AAAAAA" }
+			hl.LineNrAbove = { fg = "#AAAAAA" }
+			hl.LineNrBelow = { fg = "#AAAAAA" }
+		end,
+	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
+		vim.cmd([[colorscheme catppuccin-mocha]])
+	end,
 }
